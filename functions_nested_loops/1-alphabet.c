cf3350fc6f89343521544320ffb4df910fdb3979
ydroal <5617@holbertonstudents.com>
2022-10-04 14:40:20 -0700

    task1_1st

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_alphabet - prints the alphabet, in lowercase
+  * Return: void 
+  */
+void print_alphabet(void)
+{
+	char lowcase;
+
+	lowcase = 'a';
+	while (lowcase <= 'z')
+	{
+		_putchar(lowcase);
+		lowcase++;
+	}
+	_putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 /**
   * File: main.h
   * Auth: Yoko ONO
   *
+  * Header file containing the prototypes of all functions for this projects.
   */
 int _putchar(char c);
+void print_alphabet(void);
 #endif
