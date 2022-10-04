b67ec60b72305110b93e7d39b5c816d46a219117
ydroal <5617@holbertonstudents.com>
2022-10-04 08:40:38 -0700

    modify main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,13 +1,9 @@
-#include <unistd.h>
-
+#ifndef MAIN.H
+#define MAIN.H
 /**
-  * _putchar - writes the character c to stdout
-  * @c: The character to print
+  * File: main.h
+  * Auth: Yoko ONO
   *
-  * Return: On success 1.
-  * On error, -1 is returned, and errno is set appropriately.
   */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+#endif
