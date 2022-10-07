43d413fd466b0099a9e5a3875e17c3e23325c6a5
ydroal <5617@holbertonstudents.com>
2022-10-07 04:35:47 -0700

    fix Betty coding style

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if ( j > 9)
+			if (j > 9)
 			{
 				_putchar(j / 10 + '0');
 			}
 		_putchar(j % 10 + '0');
 		}
-	_putchar('\n');	
+	_putchar('\n');
 	}
 }
