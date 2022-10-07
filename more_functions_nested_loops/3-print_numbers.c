e414a66ce2a6ffde8c48e3f40afa03a1cebd7d46
ydroal <5617@holbertonstudents.com>
2022-10-07 02:39:58 -0700

    fix Betty

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,11 @@
 /**
   * print_numbers - prints the numbers, from 0 to 9.
   *
-  * @a: The first integer to be calculated.
-  * @b: The second integer to be calculated.
   * Return: calculation result.
   */
 void print_numbers(void)
 {
 	int i = 0;
-
 	while (i <= 9)
 	{
 		_putchar(i + '0');
