e459539a63da591faba421230ea141c034441643
ydroal <5617@holbertonstudents.com>
2022-10-07 12:51:16 -0700

    fix variable name

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,9 +11,9 @@ void times_table(void)
 	int column;
 	int values;
 
-	for (column = 0; column <= 9; column++)
+	for (row = 0; row <= 9; row++)
 	{
-		for (row = 0; row <= 9; row++)
+		for (column = 0; column <= 9; column++)
 		{
 			values = row * column;
 			if (values >= 10)
