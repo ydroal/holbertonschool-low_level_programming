24412648e28d4a4ee467562f8b4d1ba628500885
ydroal <5617@holbertonstudents.com>
2022-10-07 08:03:27 -0700

    task8_2nd

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,11 +10,16 @@ void print_square(int size)
 {
 	int i;
 	int j;
-
-	for (i = 0; i < size; i++)
+	
+	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
-			_putchar('#');
-		_putchar('\n');
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
+	else
+		_putchar('\n');
 }
