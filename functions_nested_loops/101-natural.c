9401ce8d360c9298eea01141b8c42a4c0ff35649
ydroal <5617@holbertonstudents.com>
2022-10-08 16:03:46 -0700

    fix Betty coding style

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
-  * main - computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded).
+  * main - computes and prints the sum of all the multiples
+  * of 3 or 5 below 1024 (excluded).
   *
   * Return: Always 0.
   */
