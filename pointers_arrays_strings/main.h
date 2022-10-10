06153a12019a63b1c11aaeede9a1d3c3e5b01977
ydroal <5617@holbertonstudents.com>
2022-10-10 04:15:27 -0700

    task0_1st

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+  * File: main.h
+  * Auth: Yoko ONO
+  *
+  * Header file containing the prototypes of all functions for this projects.
+  */
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif
