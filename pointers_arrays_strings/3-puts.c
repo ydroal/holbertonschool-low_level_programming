6d1eb2067c4e94b1cf5ac452d28d8724dc5946cd
ydroal <5617@holbertonstudents.com>
2022-10-10 06:13:30 -0700

    fix Betty style

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * _puts - prints a string, followed by a new line, to stdout. 
+  * _puts - prints a string, followed by a new line, to stdout.
   *
   * @str: string to be printed.
   * Return: void
