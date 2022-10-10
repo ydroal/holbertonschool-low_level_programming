d2cbf5d641aa386ac6a49f6fd217bd81e3a33519
ydroal <5617@holbertonstudents.com>
2022-10-10 14:23:10 -0700

    style: to fit Betty style

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * _strlen - returns the length of a string. 
+  * _strlen - returns the length of a string.
   *
   * @s: string to be counted its length.
   * Return: the length of a string.
