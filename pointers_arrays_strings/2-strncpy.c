7d2a287e87d5fc91c97fd6ee15823046a58a52ab
ydroal <5617@holbertonstudents.com>
2022-10-11 04:34:22 -0700

    p2_task2_1st

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * _strncpy - copy at most len characters from src into dst.
+  * If src is less than len characters long, 
+  * the remainder of dst is filled with ‘\0’
+  *
+  * @dest: destination
+  * @src: string to be copied
+  * Return: the pointer to dest
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
