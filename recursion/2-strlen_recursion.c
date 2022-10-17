42c98092b2fe0e0e9d9fae998121f6993b54921f
ydroal <5617@holbertonstudents.com>
2022-10-17 06:39:44 -0700

    task2_1st

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _strlen_recursion - returns the length of a string.
+  *
+  * @s: string to be counted its length.
+  * Return: length of string
+  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_renursion(s + 1));
+}
