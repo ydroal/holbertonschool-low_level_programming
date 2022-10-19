364c5b38d6bc4291aeb5f665ceb3636e7b0f5aa0
ydroal <5617@holbertonstudents.com>
2022-10-19 13:53:54 -0700

    task3_1st

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - prints its name
+  *
+  * @argc: number of command line arguments
+  * @argv: An array containing the program command line arguments
+  * Return: Always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", x * y);
+	return (0);
+}
