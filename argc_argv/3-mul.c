9b23bcadd47834850d9e89d7b2acfff833dcfdd3
ydroal <5617@holbertonstudents.com>
2022-10-19 14:01:00 -0700

    task3_2nd

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 {
 	int x, y;
 
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
 	printf("%d\n", x * y);
 	return (0);
 }
