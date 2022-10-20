1c18134a2ed1ca911c179a708476ebe312da7aca
ydroal <5617@holbertonstudents.com>
2022-10-20 00:24:58 -0700

    fix: description of main function

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  * main - prints its name
+  * main - prints the number of arguments passed into it.
   *
   * @argc: number of command line arguments
   * @argv: An array containing the program command line arguments
