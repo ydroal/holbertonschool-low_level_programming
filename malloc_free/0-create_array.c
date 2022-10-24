94b22c748cd6d45e652fae7b0734fd1038c942e4
ydroal <5617@holbertonstudents.com>
2022-10-24 07:43:19 -0700

    task0_1st

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * create_array - creates an array of chars,
+  *		   and initializes it with a specific char
+  *
+  * @size: length of array to create.
+  * @c: char to initializes an array.
+  * Return: pointer to the array
+  *	    NULL if it fails
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i;
+
+	s = malloc(sizeof(*s) * size);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < 98; i++)
+		s[i] = c;
+	return (s);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+  * File: main.h
+  * Auth: Yoko ONO
+  *
+  * Header file containing the prototypes of all functions for this projects.
+  */
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+#endif
