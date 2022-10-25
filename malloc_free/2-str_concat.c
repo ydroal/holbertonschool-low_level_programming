bf6d64e794ddeb382590bc658edd3456acacba86
ydroal <5617@holbertonstudents.com>
2022-10-25 03:11:32 -0700

    task2_4th

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,26 +1,25 @@
-#include "main.h"                                                                                                 
-#include <stdlib.h>                                                                                               
-#include <stdio.h>                                                                                                
+#include "main.h"
+#include <stdlib.h>
 
-/**                                                                                                               
- * str_concat - concatenates two strings.                                                                        
- *                                                                                                               
- * @s1: string to be append the @s2 string                                                                       
- * @s2: string append the @s1 string.                                                                            
- * Return: pointer to concatenated string                                                                        
- *         NULL if it fails                                                                                      
- */                                                                                                              
+/**
+  * str_concat - concatenates two strings.
+  *
+  * @s1: string to be append the @s2 string
+  * @s2: string append the @s1 string.
+  * Return: pointer to concatenated string
+  *         NULL if it fails
+  */
 
-char *str_concat(char *s1, char *s2)                                                                              
-{                                                                                                                 
-	char *concat;                                                                                             
-	int len1, len2;                                                                                           
-	int i, j;                                                                                                 
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int len1, len2;
+	int i, j;
 
-	if (s1 == NULL)                                                                                           
+	if (s1 == NULL)
 		len1 = 0;
 	else
-	{	
+	{
 		for (len1 = 0; s1[len1]; len1++)
 			;
 	}
@@ -32,20 +31,19 @@ char *str_concat(char *s1, char *s2)
 		for (len2 = 0; s2[len2]; len2++)
 			;
 	}
-
-	concat = malloc(sizeof(char) * (len1 + len2 + 1));                                                        
-	
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (concat == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1 ; i++)                                                                               
-	{                                                                                                         
-		concat[i] = s1[i];                                                                                
-	}                                                                                                         
-	for (j = 0; j < len2 ; j++)                                                                               
-	{                                                                                                         
-		concat[i + j] = s2[j];                                                                            
-	}                                                                                                         
+	for (i = 0; i < len1 ; i++)
+	{
+		concat[i] = s1[i];
+	}
+	for (j = 0; j < len2 ; j++)
+	{
+		concat[i + j] = s2[j];
+	}
 	concat[i + j] = '\0';
-	return (concat);                                                                                                                                                                                
-}                              
+	return (concat);
+	free(concat);
+}
