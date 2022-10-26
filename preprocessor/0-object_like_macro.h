c1210cbd3985d3a59050eca9b21eafd89c87de66
ydroal <5617@holbertonstudents.com>
2022-10-26 09:15:41 -0700

    task0_1st

---
+++ preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
+/**
+ * File: 0-object_like_macro.h
+ * Auth: Yoko ONO
+ *
+ * Header file containing the definition of macros.
+ */
+#define SIZE 1024
+#endif
