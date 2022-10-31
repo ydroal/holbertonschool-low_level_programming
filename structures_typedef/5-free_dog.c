c0cef908bd1eca92b75900ee1b26a61ab2d459a5
ydroal <5617@holbertonstudents.com>
2022-10-31 12:16:15 -0700

    task5_2nd

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * free_dog - frees dogs.
@@ -11,7 +10,10 @@
 
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
