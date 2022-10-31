8a69b23ffaf10511ca813c715f8c4447f2ad83b5
ydroal <5617@holbertonstudents.com>
2022-10-31 08:48:44 -0700

    task4_2nd

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,51 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: string to be counted its length.
+ * Return: the length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ *
+ * @str: string to be copied to newly allocated space in memory
+ * Return: pointer to the duplicated string
+ *	    NULL if it fails
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	int len;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = _strlen(str);
+
+	dup = malloc(sizeof(*dup) * len + 1);
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len ; i++)
+		dup[i] = str[i];
+
+	dup[i] = '\0';
+	return (dup);
+}
 
 /**
  * new_dog - creates a new dog.
@@ -14,10 +59,26 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	char *dup_name;
+	char *dup_owner;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
+
+	dup_name = _strdup(name);
+	if (dup_name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dup_owner = _strdup(owner);
+	if (dup_owner == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 
 	dog->name = name;
 	dog->age = age;
