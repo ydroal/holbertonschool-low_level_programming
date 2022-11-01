c4f2e344f0fbbae456f45ccb1fd6170e5148e36d
ydroal <5617@holbertonstudents.com>
2022-10-31 17:05:30 -0700

    task0_2nd

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stddef.h>
 /**
  * print_name - prints a name.
  *
@@ -10,5 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
 }
