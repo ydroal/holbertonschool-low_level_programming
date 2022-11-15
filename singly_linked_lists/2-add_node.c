99f35472ce483a9bfcbf3d2582e27d7b238f0c9d
ydroal <5617@holbertonstudents.com>
2022-11-15 02:58:11 -0800

    add: 2-add_node.c, update: lists.h

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: string to be counted its length.
+ * Return: the length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+  * add_node - adds a new node at the beginning of a list_t list.
+  *
+  * @head: the head address of the list
+  * @str: strings to set to the new element
+  * Return: the address of the new element,
+  *	    or NULL if it failed.
+  */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	char *tmp;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	tmp = strdup(str);
+	if (tmp == NULL)
+		free(tmp);
+
+	new->str = tmp;
+	new->len = _strlen(tmp);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -20,5 +24,5 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-
+list_t *add_node(list_t **head, const char *str);
 #endif
