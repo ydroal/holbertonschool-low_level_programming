bbe6b6ea15d7d496405b2e69bb69402a73bc4373
ydroal <5617@holbertonstudents.com>
2022-11-16 11:22:50 -0800

    modify: 5-free_listint2.c

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
+
 	while (*head != NULL)
 	{
 		tmp = *head;
 		*head = tmp->next;
 		free(tmp);
 	}
-	*head = NULL;
 }
