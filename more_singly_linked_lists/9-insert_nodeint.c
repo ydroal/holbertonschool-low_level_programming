52490b73f446863a3fb8584389837ede5dea2031
ydroal <5617@holbertonstudents.com>
2022-11-17 03:28:50 -0800

    modify: 9-insert_nodeint.c

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	else if (idx == 0)
 	{
-		new->next = *head;
+		new->next = node;
 		*head = new;
 		return (new);
 	}
