e3f578ce0dd9e16f6023eeefc455c9000397d22e
ydroal <droal.yoko@gmail.com>
2022-11-21 16:10:16 -0800

    task1_2nd

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -32,9 +32,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	if (text_content != NULL)
 	{
@@ -44,5 +44,6 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 	}
 
+	close(fd);
 	return (1);
 }
