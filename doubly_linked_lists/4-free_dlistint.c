184cecba36b4f17203eef879dd655dae84f9777b
ydroal <droal.yoko@gmail.com>
2022-11-22 06:43:05 -0800

    task4_1st

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * free_dlistint - frees a dlistint_t list.
+  *
+  * @head: pointer to dlistint_t
+  * Return: Nothing
+  */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
