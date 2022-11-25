397e484f9c1fd6a356f89af1a65ab07fd55ebd66
ydroal <droal.yoko@gmail.com>
2022-11-25 14:23:49 -0800

    task3_2nd

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,38 @@
 #include "hash_tables.h"
 
 /**
+ * hash_node_create - create the node to set to hash table
+ *
+ * @key: the key
+ * @value: the value for the key
+ *
+ * Return: pointer to new node
+ */
+hash_node_t *hash_node_create(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
+	new->next = NULL;
+	return (new);
+}
+
+/**
  * hash_table_set - adds an element to the hash table.
  *
  * @ht: pointer to hash table
@@ -12,7 +44,8 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index, i;
+	unsigned long int index;
+	hash_node_t *node;
 	hash_node_t *new;
 	char *copy_value;
 
@@ -24,28 +57,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	new = ht->array[index];
+	node = ht->array[index];
 
-	for (i = index; ht->array[i]; i++)
+	if (node == NULL)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		ht->array[index] = hash_node_create(key, value);
+		return (1);
+	}
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = copy_value;
+			free(node->value);
+			node->value = copy_value;
 			return (1);
 		}
+		node = node->next;
 	}
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (0);
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = copy_value;
+	new = hash_node_create(key, value);
 	new->next = ht->array[index];
 	ht->array[index] = new;
 	return (1);
