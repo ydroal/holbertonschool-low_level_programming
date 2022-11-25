1cf7a01245edd29783cf81df74dcc71a57ec2c86
ydroal <droal.yoko@gmail.com>
2022-11-25 15:19:16 -0800

    task4_2nd

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -14,6 +14,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (ht == NULL || *key == '\0' || key == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 
