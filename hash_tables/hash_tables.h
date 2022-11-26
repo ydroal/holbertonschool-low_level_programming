dc5f8c2d4d5ccb41503d7c847eb52a00e94d5513
ydroal <droal.yoko@gmail.com>
2022-11-26 06:27:30 -0800

    update: hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -42,4 +42,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 hash_node_t *hash_node_create(const char *key, const char *value);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 #endif
