5bcd7dcd8b41527f4cf8f6668cf4d0430d22b11e
ydroal <droal.yoko@gmail.com>
2022-11-27 02:11:27 -0800

    task3_1st

---
+++ file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+  * main - copies the content of a file to another file
+  *
+  * @argc: argument count
+  * @argv: source file and destination file
+  * Return: 0 if success
+  */
+int main(int argc, char *argv[])
+{
+	int fd_f, fd_t, w, r, c_fdf, c_fdt;
+	char buff[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_f = open(argv[1], O_RDONLY);
+	if (fd_f == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+
+	fd_t = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	while ((r = read(fd_f, buff, 1024)) > 0)
+	{
+		w = write(fd_t, buff, r);
+		if (fd_t == -1 || w < 0)
+		{
+			dprintf(STDERR_FILENO, "Can't write to NAME_OF_THE_FILE\n");
+			exit(99);
+		}
+	}
+
+	c_fdf = close(fd_f);
+	c_fdt = close(fd_t);
+	if (c_fdf == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd_f);
+		exit(100);
+	}
+	if (c_fdt == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd_t);
+		exit(100);
+	}
+	return (0);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
