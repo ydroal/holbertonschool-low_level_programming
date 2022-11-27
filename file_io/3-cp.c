a4310f4bf0af8bffeccd410ad18018c0b8a35c66
ydroal <droal.yoko@gmail.com>
2022-11-27 03:17:10 -0800

    task3_5th

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
   * main - copies the content of a file to another file
   *
@@ -17,25 +16,27 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_f = open(argv[1], O_RDONLY);
+	if (fd_t == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 	if (fd_f == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_t = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((r = read(fd_f, buff, 1024)) > 0)
 	{
 		w = write(fd_t, buff, r);
-		if (fd_t == -1 || w < 0)
+		if (w < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-
 	if (close(fd_f) == -1 || close(fd_t) == -1)
 	{
 		if (close(fd_f) == -1)
